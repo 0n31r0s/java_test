077cf63bbf71d19adb9c39af64871068dda7782e
Vangel Krstevski <vangel@pickatale.com>
2016-05-17 12:22:26 +0200

    new commit

--- src/java_test_project/klasata.java
+++ src/java_test_project/klasata.java
@@ -3,7 +3,7 @@ package java_test_project;
 public class klasata {
 
 	public static void main(String[] args) {
-		System.out.println("Vo ... Dobar den");
+		System.out.println("Vo ... Dobar den !!!!");
 
 	}
 
